b9d0de974d6ef4bfbba87604f6e47c1a6fc6d566
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2026-02-03 18:45:18 -0500

    fixed the aws onboarding module to have correct name and tags

--- aws_account_onboarding.tf
+++ aws_account_onboarding.tf
@@ -2,7 +2,7 @@ module "aws_account_onboarding" {
   source  = "illumio/cloudsecure/illumio//modules/aws_account"
   version = "~>1.6.4"
   name    = "AWS Insights Unified-Shared2[ Do not delete ]"
-  iam_name_prefix       = var.regional_demo_name
+  iam_name_prefix       ="${var.regional_demo_name}${random_id.random_id.hex}"
   mode = "Read"
   tags    = {
     Name  = "Unified_Shared2 Account",
