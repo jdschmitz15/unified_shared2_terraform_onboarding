52723a57b2fca086535677fde79408100c6f2229
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2026-03-10 18:25:36 -0400

    Cleaned up the storage account list.

--- azure_flow_onboarding.tf
+++ azure_flow_onboarding.tf
@@ -6,36 +6,32 @@ module "azure_flow_logs_storage_accounts" {
   iam_name_prefix       = "${var.regional_demo_name}${random_id.random_id.hex}"
   storage_accounts = [
     {
-      name                = "diag11f0a481ac028e33"
-      resource_group_name = "testdrive"
-    },
-  {
-      name                = "illumiosa333ee6westus"
-      resource_group_name = "resourcegroup_westus"
-    },
-    {
-      name                = "illumiosad1b6bfeastus"
-      resource_group_name = "resourcegroup_eastus"
-    },
-    {
-      name                = "flowlogsadiv2"
-      resource_group_name = "flowlogstest"
-    },
-    {
-      name                = "flowlogsadinsg"
-      resource_group_name = "flowlogstest"
+      name                = "trafficgenadi"
+      resource_group_name = "trafficgen"
     } 
   ]
+# This is the original list of storage accounts.  These will all be deleted.
+# storage_accounts = [
+#     {
+#       name                = "diag11f0a481ac028e33"
+#       resource_group_name = "testdrive"
+#     },
+#   {
+#       name                = "illumiosa333ee6westus"
+#       resource_group_name = "resourcegroup_westus"
+#     },
+#     {
+#       name                = "illumiosad1b6bfeastus"
+#       resource_group_name = "resourcegroup_eastus"
+#     },
+#     {
+#       name                = "flowlogsadiv2"
+#       resource_group_name = "flowlogstest"
+#     },
+#     {
+#       name                = "flowlogsadinsg"
+#       resource_group_name = "flowlogstest"
+#     } 
+#   ]
 
-  # Test with additional account and storage accounts
-  # storage_accounts = [
-  #   {
-  #     name                = "flowlogs3z4febpq2olsk"
-  #     resource_group_name = "jds-trial"
-  #   },
-  #   {
-  #     name                = "illumiosaf7cbc7eastus2"
-  #     resource_group_name = "illumioresourcegroup_eastus2"
-  #   }
-  # ]
 }
